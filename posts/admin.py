ef0e64d25ea66ba6790c59df1fdd7714a584d1d8
Amir Tavakoli <a_tavakoli@arsh.co>
2023-11-16 12:05:18 +0330

    Update admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    ordering = ['-updated_at']
+    list_display = ['title', 'created_at']
+    list_filter = ['user']
+    search_fields = ['text']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Post, PostAdmin)
+
 
