10beb85225da4e4a04e86d5efa7eccb90ccbe7a4
Amir Tavakoli <a_tavakoli@arsh.co>
2024-01-11 00:32:36 +0330

    test locale

--- users/models.py
+++ users/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_fsm import transition, FSMField
 from .constants import TicketState
+from django.utils.translation import gettext_lazy as _
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name=_("User"))
+    first_name = models.CharField(max_length=50, verbose_name=_("First Name"))
+    last_name = models.CharField(max_length=50, verbose_name=_("Last Name"))
     personal_id = models.IntegerField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name_plural = 'کاربران'
+        verbose_name_plural = _("Users")
 
     def __str__(self):
         return f'{self.personal_id}'
